516681bf9fef4c595abe8cee842c9be67b0cabf3
Max Esnée <contact@max-esnee.com>
2025-09-26 22:42:22 +0200

    Add basic css reset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  css: ['~/assets/css/main.css'],
+  css: ['~/assets/css/reset.css', '~/assets/css/main.css'],
   devtools: { enabled: true },
   runtimeConfig: {
     apiAuthEmail: '',
