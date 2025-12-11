7eba0b80f890abc65c3058adec1f0740c2140c11
Max Esnée <contact@max-esnee.com>
2025-12-11 16:03:57 +0100

    Remove test code

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -14,24 +14,6 @@
             <Section id="objects" />
          </template>
       </SplitScroll>
-      <SplitScroll>
-         <template v-slot:left>
-            <h3>Blabla à gauche</h3>
-            <div>
-               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem enim tempora earum facere quo, adipisci inventore esse saepe mollitia tempore similique molestias animi optio. Sunt eos doloremque sint et voluptatibus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem enim tempora earum facere quo, adipisci inventore esse saepe mollitia tempore similique molestias animi optio. Sunt eos doloremque sint et voluptatibus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem enim tempora earum facere quo, adipisci inventore esse saepe mollitia tempore similique molestias animi optio. Sunt eos doloremque sint et voluptatibus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem enim tempora earum facere quo, adipisci inventore esse saepe mollitia tempore similique molestias animi optio. Sunt eos doloremque sint et voluptatibus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem enim tempora earum facere quo, adipisci inventore esse saepe mollitia tempore similique molestias animi optio. Sunt eos doloremque sint et voluptatibus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem enim tempora earum facere quo, adipisci inventore esse saepe mollitia tempore similique molestias animi optio. Sunt eos doloremque sint et voluptatibus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem enim tempora earum facere quo, adipisci inventore esse saepe mollitia tempore similique molestias animi optio. Sunt eos doloremque sint et voluptatibus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem enim tempora earum facere quo, adipisci inventore esse saepe mollitia tempore similique molestias animi optio. Sunt eos doloremque sint et voluptatibus!
-            </div>
-         </template>
-         <template v-slot:right>
-            <h3>Blabla à droite</h3>
-            <div>
-               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem enim tempora earum facere quo, adipisci inventore esse saepe mollitia tempore similique molestias animi optio. Sunt eos doloremque sint et voluptatibus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem enim tempora earum facere quo, adipisci inventore esse saepe mollitia tempore similique molestias animi optio. Sunt eos doloremque sint et voluptatibus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem enim tempora earum facere quo, adipisci inventore esse saepe mollitia tempore similique molestias animi optio. Sunt eos doloremque sint et voluptatibus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem enim tempora earum facere quo, adipisci inventore esse saepe mollitia tempore similique molestias animi optio. Sunt eos doloremque sint et voluptatibus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem enim tempora earum facere quo, adipisci inventore esse saepe mollitia tempore similique molestias animi optio. Sunt eos doloremque sint et voluptatibus!
-            </div>
-            <h4>Sous-titre</h4>
-            <div>
-               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem enim tempora earum facere quo, adipisci inventore esse saepe mollitia tempore similique molestias animi optio. Sunt eos doloremque sint et voluptatibus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem enim tempora earum facere quo, adipisci inventore esse saepe mollitia tempore similique molestias animi optio. Sunt eos doloremque sint et voluptatibus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem enim tempora earum facere quo, adipisci inventore esse saepe mollitia tempore similique molestias animi optio. Sunt eos doloremque sint et voluptatibus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem enim tempora earum facere quo, adipisci inventore esse saepe mollitia tempore similique molestias animi optio. Sunt eos doloremque sint et voluptatibus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem enim tempora earum facere quo, adipisci inventore esse saepe mollitia tempore similique molestias animi optio. Sunt eos doloremque sint et voluptatibus!
-            </div>
-         </template>
-      </SplitScroll>
       <Section id="outro" />
 	</main>
 </template>
