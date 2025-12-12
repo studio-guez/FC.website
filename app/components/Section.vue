73f52b75fddacbfba9f632c8c17e2827e0141227
Max Esnée <contact@max-esnee.com>
2025-12-12 17:57:22 +0100

    Implement mobile option for blocks

--- app/components/Section.vue
+++ app/components/Section.vue
@@ -5,17 +5,17 @@
          <div v-if="block.type === 'text'" class="text mb-1" :class="{mobile: block.mobile}" v-html="block.text" :style="{color: block.color}"/>
 
          <!-- Image -->
-         <div v-if="block.type === 'image'" :class="['image', 'mb-1']" :style="{color: block.color, borderRadius: `${block.cornerRadius*5}px`}">
+         <div v-if="block.type === 'image'" class="image mb-1" :class="{mobile: block.mobile}" :style="{color: block.color, borderRadius: `${block.cornerRadius*5}px`}">
             <img :src="block.image.url" :alt="block.image.alt">
          </div>
 
          <!-- File -->
-         <div v-if="block.type === 'file'" class="file mb-1" :style="{color: block.color}">
+         <div v-if="block.type === 'file'" class="file mb-1" :class="{mobile: block.mobile}" :style="{color: block.color}">
             <a :href="block.file.url" target="_blank">{{ block.label }}</a>
          </div>
 
          <!-- Link -->
-         <div v-if="block.type === 'link'" class="link mb-1" :style="{color: block.color}">
+         <div v-if="block.type === 'link'" class="link mb-1" :class="{mobile: block.mobile}" :style="{color: block.color}">
             <a :href="block.url" target="_blank">{{ block.label }}</a>
          </div>
 
@@ -25,7 +25,7 @@
           </section>
 
          <!-- Separator -->
-         <hr v-if="block.type === 'hr'" class="hr mb-1" :style="{color: block.color}" />
+         <hr v-if="block.type === 'hr'" class="hr mb-1" :class="{mobile: block.mobile}" :style="{color: block.color}" />
 
          <!-- Newsletter -->
          <div v-if="block.type === 'newsletter'" class="newsletter mb-1" :style="{color: block.color}">
